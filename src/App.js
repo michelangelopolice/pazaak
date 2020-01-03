c678d1a35c9bdd58d06558ea0a7c999aee45dfe4
Victor <doctorhow23@googlemail.com>
2020-01-03 12:17:20 +0000

    testing grid with css
    testing grid with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
-import Board from './components/Board';
-import Grid from '@material-ui/core/Grid';
+// import Board from './components/Board';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pazaak</h1>
+        <h1>Main Title</h1>
         <p>
-          A game of champions.
+          Description here
         </p>
-        <div>
-          <Grid container>
-            <Board className="board" />
-            <Board className="board" />
-          </Grid>
-        </div>
       </header>
+      <main className="App-main container">
+        <div className="item-a">item-a</div>
+        <div className="item-b">item-b</div>
+        <div className="item-c">item-c</div>
+        <div className="item-d">item-d</div>
+        <div className="item-e">item-e</div>
+        <div className="item-f">item-f</div>
+        <div className="item-g">item-g</div>
+        <div className="item-h">item-h</div>
+        <div className="item-i">item-i</div>
+      </main>
     </div>
   );
 }
